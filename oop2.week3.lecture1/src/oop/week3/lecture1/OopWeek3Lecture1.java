4cebf42a8ab892a02288d702240d912e3dfde95e
SWohoush <148148647+SWohoush@users.noreply.github.com>
2023-10-30 18:58:12 +0300

    Update OopWeek3Lecture1.java

--- oop2.week3.lecture1/src/oop/week3/lecture1/OopWeek3Lecture1.java
+++ oop2.week3.lecture1/src/oop/week3/lecture1/OopWeek3Lecture1.java
@@ -1,19 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package oop.week3.lecture1;
 
-/**
- *
- * @author sohair
- */
 public class OopWeek3Lecture1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Person p1=new Person();
         p1.age=100;
