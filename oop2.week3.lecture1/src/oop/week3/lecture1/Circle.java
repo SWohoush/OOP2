044ef6640824ec222d3ac3d297c3bc3e19a3a150
SWohoush <148148647+SWohoush@users.noreply.github.com>
2023-10-30 18:58:46 +0300

    Update Circle.java

--- oop2.week3.lecture1/src/oop/week3/lecture1/Circle.java
+++ oop2.week3.lecture1/src/oop/week3/lecture1/Circle.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package oop.week3.lecture1;
-import java.lang.Math.*;
-/**
- *
- * @author sohair
- */
+
 public class Circle {
     int radius;
     
@@ -18,4 +9,5 @@ public class Circle {
     public double getArea(){
         return Math.PI*radius*radius;
     }
+    
 }
