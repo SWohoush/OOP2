736e52e9e58e28fdc3d3eace09365ebf2107f52b
SWohoush <148148647+SWohoush@users.noreply.github.com>
2023-11-28 23:53:34 +0300

    Delete oop2.week7.lecture1/src/oop2/week7/lecture1 directory

--- oop2.week7.lecture1/src/oop2/week7/lecture1/FacadeTest.java
+++
@@ -1,9 +0,0 @@
-package oop2.week7.lecture1;
-
-public class FacadeTest {
-
-    public static void main(String[] args) {
-        Facade facade = new Facade();
-        facade.operate();
-    }
-}
--- oop2.week7.lecture1/src/oop2/week7/lecture1/Subsystem1.java
+++
@@ -1,8 +0,0 @@
-package oop2.week7.lecture1;
-
-public class Subsystem1 {
-
-    public void operation1() {
-        System.out.println("Subsystem1:operation1");
-    }
-}
